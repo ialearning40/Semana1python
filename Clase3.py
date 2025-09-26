e58a3e52fe451598c4dc701ff2087730507f22cc
unknown <ialearning@gmail.com>
2025-09-26 09:52:21 -0500

    Clase3 - Curso Pytoh

---
+++ Clase3.py
@@ -0,0 +1,50 @@
+saludo1 = "hola"
+saludo2 = '''hi
+Tatiana'''
+nombre = 'Tata'
+edad = "'40'"
+#print (saludo2)
+#print (nombre)
+#print (type (edad))
+# Indexacion
+# COnsultar la posicion 
+name = 'Tatiana Mo'
+#print (name[-1])
+# Concanetacion 
+name = [1, 2, 3, 2]
+name2 = name.count(2)
+last_name = 'Tata Jimenez'
+#print(name + " "+ last_name)
+#print(name *5)
+#print (len(name))
+# metodos
+#print(name.strip())
+#print (f"el numero 2 {name2}")
+list = "hola tata"
+#print (" ".join(list))
+#print(list.find("tata"))
+# Clases datos numero flotante, entero y boleano
+x=10
+
+y = 1.2e6
+print(y)
+print(type(y))
+
+# Nuevo ejercicio operadores numericos
+a = 10
+b = 3
+#print (a+b)
+#print (a-b)
+#print (a*b)
+#print (a/b)
+
+# tener la parte entera de la division
+print ("modulo", a % b)
+print (a//b)
+print (a**b)
+
+#shortcuts
+#a = a + 2
+#print (a)
+a -= 3
+print (a)
